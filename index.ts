7d71cb90e7915582c99616e4bc4a2a1a4a1debb9
mama omo <team@omo.earth>
2020-10-25 19:43:39 +0100

    Added Apiresponse types

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 export * from './src/interfaces/Registry';
 
 
-
 export * from "./src/types/Constructor";
 export * from "./src/types/RouteArguments";
-
-
+export * from "./src/types/ApiResponse";
+export * from "./src/types/User";
 
 
 export * from "./src/classes/Router";
-
-
 
 
 export * from "./src/enums/routes"
